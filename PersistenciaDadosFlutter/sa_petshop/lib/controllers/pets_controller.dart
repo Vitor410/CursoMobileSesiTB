91d9755745432b9eecd3639ff19a91b328ed76d5
Vitor410 <vitor.rossi.scholl@gmail.com>
2025-05-16 16:56:07 -0300

    16.05.25

---
+++ PersistenciaDadosFlutter/sa_petshop/lib/controllers/pets_controller.dart
@@ -0,0 +1,24 @@
+import 'package:sa_petshop/database/db_helper.dart';
+import 'package:sa_petshop/models/pet_model.dart';
+
+class PetsController {
+  //atributo -> 
+  final PetShopDBHelper _dbHelper = PetShopDBHelper();
+
+
+  Future<int> addPet(Pet pet) async {
+    return await _dbHelper.insertPet(pet);
+  }
+
+  Future<List<Pet>> fetchPets() async {
+    return await _dbHelper.getPets();
+  }
+
+  Future<Pet?> findPetById(int id) async {
+    return await _dbHelper.getPetById(id);
+  }
+
+  Future<int> deletePet(int id) async {
+    return await _dbHelper.deletePet(id);
+  }
+}
