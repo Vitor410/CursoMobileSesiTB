f331b622dde93ea748a94bfd3b78bf09e6112c32
Vitor410 <vitor.rossi.scholl@gmail.com>
2025-05-23 16:52:22 -0300

    23.05.25

--- PersistenciaDadosFlutter/sa_petshop/lib/controllers/consultas_controller.dart
+++ PersistenciaDadosFlutter/sa_petshop/lib/controllers/consultas_controller.dart
@@ -5,14 +5,14 @@ class ConsultasController {
   final PetShopDBHelper _dbHelper = PetShopDBHelper();
 
   Future<List<Consulta>> getConsultasByPet(int petId) async{
-    return await _dbHelper.getConsultasForPet(petId);
+    return await _dbHelper.getConsultaForPet(petId);
   }
 
   Future<int> insertConsulta(Consulta consulta) async{
     return await _dbHelper.insertConsulta(consulta);
   }
 
-  Future<int> deleteConsulta(int id) async {
+  Future<int> deleteConsulta(int id) async{
     return await _dbHelper.deleteConsulta(id);
   }
 }
